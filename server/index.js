d071d579fc88cffaa50afd4e40ab8866d8a56501
aftabkhan1234 <aftabkhan@gmail.com>
2023-12-09 22:58:21 +0530

    com

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ const corsOptions = {
     credentials: true
 }
 
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*'); 
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 app.use(cors(corsOptions))
 app.use(express.json())
